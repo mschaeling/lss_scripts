cb1c6ea55adb695691bb643a3172744eecf8c99c
mschaeling <mail@schaeling.com>
2020-05-16 10:26:11 +0200

    Update LSS Mission Details.user.js

--- LSS Mission Details.user.js
+++ LSS Mission Details.user.js
@@ -11,8 +11,9 @@
 const STYLE = `
 <style>
 #mission_detail_container { display: flex; padding: 1em 0; background-color: #efefef: }
-.mission_detail_tab { width: 33%; border: 1px solid; }
+.mission_detail_tab { width: 33%; }
 .mission_detail_tab td { padding: 0 .5em; }
+.mission_detail_tab tr td:first-child { font-size: .7em; width: 30%; }
 .mission_detail_tab th { display: none; }
 </style>
 `;
@@ -39,7 +40,9 @@ const COLUMN = {'prefix': '<div class="mission_detail_tab"><table>', 'suffix': '
             var html = '';
 
             $($.parseHTML(data)).filter('#iframe-inside-container').find('.row table').each(function(e){
-                html += COLUMN.prefix + $(this).html() + COLUMN.suffix;
+                if (e > 0) {
+                    html += COLUMN.prefix + $(this).html() + COLUMN.suffix;
+                }
             });
 
             contents.find("#mission_detail_container").html(html);
