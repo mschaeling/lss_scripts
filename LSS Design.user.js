bc04839405bc249047977e41b27e86400f74c958
mschaeling <mail@schaeling.com>
2020-05-16 09:12:09 +0200

    Update LSS Design.user.js

--- LSS Design.user.js
+++ LSS Design.user.js
@@ -11,7 +11,17 @@
 
 GM_addStyle(`
 
-#vhc_bar { display: none; }
+#vhc_bar {
+position: fixed;
+top: calc(51px + 1em);
+left: 1em;
+z-index: 401;
+padding: .25em;
+background-color: #FFF;
+-webkit-box-shadow: 2px 2px 8px 0px rgba(0,0,0,0.75);
+-moz-box-shadow: 2px 2px 8px 0px rgba(0,0,0,0.75);
+box-shadow: 2px 2px 8px 0px rgba(0,0,0,0.75);
+}
 
 body, html {
   display: flex;
@@ -26,13 +36,34 @@ body, html {
 
 #main_navbar {
   margin: 0;
+border: none;
 }
 
+.navbar-default {
+border-color: #c9302c;
+}
+
+#main_navbar {
+-webkit-box-shadow: 2px 2px 8px 0px rgba(0,0,0,0.75);
+-moz-box-shadow: 2px 2px 8px 0px rgba(0,0,0,0.75);
+box-shadow: 2px 2px 8px 0px rgba(0,0,0,0.75);
+}
+
+#row-main-template {
+display: contents;
+}
 #main_container {
   overflow: hidden;
   flex-grow: 1;
   display: flex;
   flex-direction: column;
+}
+
+#main_container > .alert {
+position: fixed;
+    top: calc(51px + 1em);
+    left: 5em;
+    z-index: 402;
 }
 
 #content {
@@ -65,15 +96,47 @@ left: 1em;
 }
 
 
-#important_messages_success,
+#important_messages_success {
+position: fixed;
+    bottom: 6em;
+    width: 80%;
+    left: 1em;
+    top: unset;
+max-width: calc(66.66% - 1em);
+}
 .client-id {
   display: none;
 }
 
+/* Hide scrollbar for Chrome, Safari and Opera */
+.widget::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE and Edge */
 .widget {
+  -ms-overflow-style: none;
+}
+
+.widget, #vhc-bar {
 -webkit-box-shadow: 2px 2px 8px 0px rgba(0,0,0,0.75);
 -moz-box-shadow: 2px 2px 8px 0px rgba(0,0,0,0.75);
 box-shadow: 2px 2px 8px 0px rgba(0,0,0,0.75);
+}
+
+.widget .panel-heading {
+background: none;
+border: none;
+}
+
+.widget > div > .panel,
+.widget > .panel {
+border: none;
+}
+
+.widget .panel-body {
+padding-left: 0;
+padding-right: 0;
 }
 
 .widget-toggler {
@@ -119,8 +182,88 @@ box-shadow: 2px 2px 8px 0px rgba(0,0,0,0.75);
   display: none;
 }
 
+#building_panel_heading,
+#chat_panel_heading {
+    padding: 0;
+    display: flex;
+}
+
+#building_panel_heading .btn-group,
+#chat_panel_heading .btn-group {
+    position: relative;
+    display: flex;
+    justify-content: stretch;
+    flex-grow: 1;
+margin: 1em 0;
+}
 
 
+#building_panel_heading .btn-group a,
+#chat_panel_heading .btn-group button {
+    flex-grow: 1;
+    border: none;
+    background: #c9302c;
+    color: #FFF;
+    border-radius: 0;
+    padding: .5em;
+}
+
+#building_panel_heading .btn-group a:hover,
+#chat_panel_heading .btn-group button:hover{
+    opacity: .8;
+}
+
+#building_panel_heading .btn-group a:first-child {
+margin-right: .5em;
+}
+
+#btn-group-building-select {
+display: flex;
+margin-bottom: 1em;
+}
+
+#buildings .building_selection {
+text-transform: uppercase;
+width: calc(100% / 7);
+}
+
+#building_panel_heading .btn-group a:last-child {
+margin-left: .5em;
+}
+
+.building_list_vehicles {
+display: flex;
+    flex-wrap: wrap;
+justify-content: space-between;
+}
+
+.building_list_vehicles a.btn.backalarm {
+    width: 100%;
+    margin-top: .25em;
+}
+
+.building_list_vehicle_element {
+width: calc(50% - .5em);
+overflow: hidden;
+border: 1px solid rgba(0,0,0,.1);
+    padding: .25em;
+}
+
+#chat_panel_body {
+padding-top: 0;
+}
+
+#chat_outer .panel-body {
+padding: 0;
+}
+
+#alliance_chat_message {
+border-radius: 0;
+}
+
+#new_alliance_chat .input-group-addon {
+display: none;
+}
 
 .alert {
   padding: .25em;
@@ -157,6 +300,7 @@ div[id^="mission_panel_"] .panel-body {
   border: none;
   padding: 0 .5em;
   background: rgba(255,255,255,.2);
+margin: 0;
 }
 
 #map_adress_search::placeholder {
@@ -195,22 +339,61 @@ div[id^="mission_patients_"] {
 
 #btn-group-mission-select {
   width: 100%;
-  padding-right: 20px;
+display: flex;
+justify-content: space-between;
+}
+
+
+#btn-group-mission-select .btn {
+  width: 20%;
+border-radius: 0;
+padding: .5em;
 }
 
 #search_input_field_missions {
   margin: .5em 0 .5em 0;
-  width: calc(100% - 1.5em);
+  width: 100%;
   border: none;
   background-color: rgba(0,0,0,.1);
-  padding: .25em .5em;
+  padding: .5em;
+border-radius: 0;
+background-image: none;
 }
 
 .missions-panel-head.big_map_window_head > strong {
   display: none;
 }
+#missions-panel-body {
+ padding: 0;
+}
 
+#missions .panel-heading {
+background: transparent !important;
+border: none;
+}
 
+#missions .panel-body .col-xs-1 {
+display: none;
+}
+
+#missions .alert {
+font-size: .8em;
+padding: .25em;
+border-radius: 0;
+}
+
+#building_panel_body {
+padding-top: 0;
+}
+
+#chat_panel_body {
+width: 100%;
+}
+
+.widget.note {
+color: #f44336;
+background-color: #ffcdd2;
+}
 
 `);
 
@@ -221,6 +404,12 @@ function shortCredits() {
 
 (function() {
     'use strict';
+
+    $('br').hide();
+
+    $('#building_panel_heading').html($('#building_panel_heading').html().split('Wachen')[1]);
+    $('#chat_panel_heading').html($('#chat_panel_heading').html().split('Chat')[0]);
+
 
     $('#search_input_field_missions').attr('placeholder', 'Einsätze suchen...');
 
@@ -246,9 +435,11 @@ function shortCredits() {
     $('#chat_outer').append('<div class="widget-toggler"><div class="widget-sticky"><span class="sticked glyphicon glyphicon-lock"></span><span class="free glyphicon glyphicon-lock"></span></div><span class="glyphicon glyphicon-comment" style="margin-right: .5em;"></span>Chat</div>').addClass('widget collapsed');
     $('#radio_outer').append('<div class="widget-toggler"><div class="widget-sticky"><span class="sticked glyphicon glyphicon-lock"></span><span class="free glyphicon glyphicon-lock"></span></div><span class="glyphicon glyphicon-bullhorn" style="margin-right: .5em;"></span>Radio</div>').addClass('widget collapsed');
 
+    $('#missions_outer').addClass('sticky');
+
     $('.widget.collapsed .widget-toggler').on('mouseover', function(e) {
         $('.widget:not(.collapsed)').addClass('collapsed');
-        $(e.target).closest('.widget').removeClass('collapsed');
+        $(e.target).closest('.widget').removeClass('collapsed note');
     });
 
     $('.widget-sticky').on('click', function(e) {
@@ -257,6 +448,19 @@ function shortCredits() {
 
     $('#map').on('mouseover', function(e) {
         $('.widget').addClass('collapsed');
+    });
+
+    $('#buildings .building_selection').each(function(){
+        $(this).html($(this).html().substr(0,3));
+    });
+
+    $("body").on('DOMSubtreeModified', "#mission_chat_messages", function() {
+        $('#chat_outer.collapsed').addClass('note');
+    });
+
+
+    $("body").on('DOMSubtreeModified', "#radio_panel_body", function() {
+        $('#radio_outer.collapsed').addClass('note');
     });
 
 })();
