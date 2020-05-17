787589352b6372456817258666dae811f05acb29
Manuel Schaeling <manuel.schaeling@platform3l.com>
2020-05-17 15:22:42 +0200

    0.4.1

--- LSS Design.user.js
+++ LSS Design.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS Design
 // @namespace    http://tampermonkey.net/
-// @version      0.41
+// @version      0.4.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.leitstellenspiel.de/
