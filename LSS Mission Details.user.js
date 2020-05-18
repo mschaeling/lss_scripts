27cc55c452e261d62bf2f0bf1b3c8fbe19aa6c73
mschaeling <mail@schaeling.com>
2020-05-18 18:42:14 +0200

    Update LSS Mission Details.user.js

--- LSS Mission Details.user.js
+++ LSS Mission Details.user.js
@@ -11,13 +11,18 @@
 
 const STYLE = `
 <style>
-#mission_detail_container { display: flex; padding: 1em 0; background-color: #efefef: }
-.mission_detail_tab { width: 50%; }
-.mission_detail_tab td { padding: 0 .5em; }
-.mission_detail_tab tr td:first-child { font-size: .7em; width: 30%; }
-.mission_detail_tab th { display: none; }
-.mission_detail_tab_percent { opacity: .5; }
-.mission_detail_tab_percent td:last-child:after { content: "%"; }
+#mission_detail_container { display: flex; flex-wrap: wrap; padding: 1em 0; background-color: #efefef; }
+#mission_detail_container .mission_detail_tab { width: 50%; background-color: #efefef; padding: .5em 0; }
+#mission_detail_container .mission_detail_tab:first-child { width: 100%; margin-bottom: 1em; }
+#mission_detail_container .mission_detail_tab:first-child table,
+#mission_detail_container .mission_detail_tab:first-child table tbody { display: flex; width: 100%; justify-content: space-between; }
+#mission_detail_container .mission_detail_tab:first-child table tbody tr,
+#mission_detail_container .mission_detail_tab:first-child table tbody tr td { display: block; }
+#mission_detail_container .mission_detail_tab td { padding: 0 .5em; }
+#mission_detail_container .mission_detail_tab:first-child tr td:first-child { font-size: .7em; }
+#mission_detail_container .mission_detail_tab th { display: none; }
+#mission_detail_container .mission_detail_tab_percent { opacity: .5; }
+#mission_detail_container .mission_detail_tab_percent td:last-child:after { content: "%"; }
 </style>
 `;
 
@@ -37,18 +42,18 @@ function normalizeTable(index, element) {
         case 1:
             element.find('tr').each(function(e){
                 if ($(this).html().includes('ahrscheinlichkeit')) {
-                    html += '<tr class="mission_detail_tab_percent">' + $(this).html().replace('Anforderungswahrscheinlichkeit', '') + '</tr>';
+                    html += '<tr class="mission_detail_tab_percent">' + $(this).html().replace('Anforderungswahrscheinlichkeit', '').replace('Drehleiter', '<b style="color: #FFC72E;">DLK</b>').replace('Rüstwagen', '<b style="color: #FFFF14;">RW</b>').replace('Löschfahrzeuge', '<b style="color: #FF0000;">LF</b>').replace('ELW 1', '<b style="color: #BE0AFF;">ELW 1</b>') + '</tr>';
                 } else {
-                    html += '<tr>' + $(this).html().replace('Benötigte', '') + '</tr>';
+                    html += '<tr>' + $(this).html().replace('Benötigte', '').replace('Rüstwagen', '<b style="color: #FFFF14;">RW</b>').replace('Drehleitern', '<b style="color: #FFC72E;">DLK</b>').replace('Löschfahrzeuge', '<b style="color: #FF0000;">LF</b>').replace('ELW 1', '<b style="color: #BE0AFF;">ELW 1</b>').replace('Streifenwagen', '<b style="color: #03D5FF;">FuStW</b>') + '</tr>';
                 }
             });
             break;
         case 2:
             element.find('tr').each(function(e){
                 if ($(this).html().includes('ahrscheinlichkeit')) {
-                html += '<tr class="mission_detail_tab_percent">' + $(this).html() + '</tr>';
+                    html += '<tr class="mission_detail_tab_percent">' + $(this).html().replace('Anforderungswahrscheinlichkeit', '').replace('Wahrscheinlichkeit, dass ein Patient transportiert werden muss', 'Transport nötig') + '</tr>';
                 } else {
-                    html += '<tr>' + $(this).html() + '</tr>';
+                    html += '<tr>' + $(this).html().replace('Fachrichtung für Patienten', 'Fachrichtung').replace('Maximale Patientenanzahl', 'Patienten MAX').replace('Mindest Patientenanzahl', 'Patienten MIN') + '</tr>';
                 }
             });
             break;
@@ -78,9 +83,11 @@ return html;
 
             $($.parseHTML(data)).filter('#iframe-inside-container').find('.row table tbody:last-child').each(function(e){
                 html += COLUMN.prefix + normalizeTable(e, $(this)) + COLUMN.suffix;
+                if (e == 0) {
                 if ($(this).html().includes('Rettungswache')) { contents.find('a[href="#rettungsdienst"]')[0].click(); }
                 if ($(this).html().includes('Feuerwache')) { contents.find('a[href="#feuerwehr_lf"]')[0].click(); }
                 if ($(this).html().includes('Polizeiwache')) { contents.find('a[href="#polizei"]')[0].click(); }
+                }
             });
 
             contents.find("#mission_detail_container").html(html);
