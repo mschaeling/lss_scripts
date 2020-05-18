f0c4d9841bc5a702bc9adfd2c32901e651eb2593
mschaeling <mail@schaeling.com>
2020-05-18 18:00:38 +0200

    Update LSS Mission Details.user.js

--- LSS Mission Details.user.js
+++ LSS Mission Details.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         LSS Mission Details
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.1.1
+// @updateUrl    https://github.com/mschaeling/lss_scripts/blob/master/LSS%20Mission%20Details.user.js
+// @description  Receive information from mission helper button
+// @author       MuckVanSchael
 // @match        https://www.leitstellenspiel.de/
 // @grant        none
 // ==/UserScript==
@@ -15,40 +16,77 @@ const STYLE = `
 .mission_detail_tab td { padding: 0 .5em; }
 .mission_detail_tab tr td:first-child { font-size: .7em; width: 30%; }
 .mission_detail_tab th { display: none; }
+.mission_detail_tab_percent { opacity: .5; }
+.mission_detail_tab_percent td:last-child:after { content: "%"; }
 </style>
 `;
 
 const WRAPPER = '<div id="mission_detail_container">lädt...</div>';
-const COLUMN = { 'prefix': '<div class="mission_detail_tab"><table>', 'suffix': '</table></div>' };
+const COLUMN = {'prefix': '<div class="mission_detail_tab"><table>', 'suffix': '</table></div>'};
+
+function normalizeTable(index, element) {
+    var html = '';
+    switch (index) {
+        case 0:
+            element.find('tr').each(function(e){
+                if ($(this).html().includes('Credits') || $(this).html().includes('Generiert')) {
+                    html += '<tr>' + $(this).html() + '</tr>';
+                }
+            });
+            break;
+        case 1:
+            element.find('tr').each(function(e){
+                if ($(this).html().includes('ahrscheinlichkeit')) {
+                    html += '<tr class="mission_detail_tab_percent">' + $(this).html().replace('Anforderungswahrscheinlichkeit', '') + '</tr>';
+                } else {
+                    html += '<tr>' + $(this).html().replace('Benötigte', '') + '</tr>';
+                }
+            });
+            break;
+        case 2:
+            element.find('tr').each(function(e){
+                if ($(this).html().includes('ahrscheinlichkeit')) {
+                html += '<tr class="mission_detail_tab_percent">' + $(this).html() + '</tr>';
+                } else {
+                    html += '<tr>' + $(this).html() + '</tr>';
+                }
+            });
+            break;
+        default:
+            break;
+            }
+return html;
+}
 
 (function() {
-  'use strict';
+    'use strict';
 
-  var old_lightboxShowClose = window.lightboxShowClose;
+    var old_lightboxShowClose = window.lightboxShowClose;
 
-  window.lightboxShowClose = function(e) {
+    window.lightboxShowClose = function(e) {
 
-    old_lightboxShowClose(e);
+        old_lightboxShowClose(e);
 
-    var contents = $("#lightbox_iframe_" + iframe_lightbox_number).contents();
+        var contents = $("#lightbox_iframe_" + iframe_lightbox_number).contents();
 
-    contents.find("head").append(STYLE);
-    contents.find("#col_right").prepend(WRAPPER);
+        contents.find("head").append(STYLE);
+        contents.find("#col_right").prepend(WRAPPER);
 
-    $.get(contents.find("#mission_help").attr('href'), function(data) {
+        $.get(contents.find("#mission_help").attr('href'), function(data){
 
-      var html = '';
+            var html = '';
 
-      $($.parseHTML(data)).filter('#iframe-inside-container').find('.row table').each(function(e) {
-        if (e > 0) {
-          html += COLUMN.prefix + $(this).html() + COLUMN.suffix;
-        }
-      });
+            $($.parseHTML(data)).filter('#iframe-inside-container').find('.row table tbody:last-child').each(function(e){
+                html += COLUMN.prefix + normalizeTable(e, $(this)) + COLUMN.suffix;
+                if ($(this).html().includes('Rettungswache')) { contents.find('a[href="#rettungsdienst"]')[0].click(); }
+                if ($(this).html().includes('Feuerwache')) { contents.find('a[href="#feuerwehr_lf"]')[0].click(); }
+                if ($(this).html().includes('Polizeiwache')) { contents.find('a[href="#polizei"]')[0].click(); }
+            });
 
-      contents.find("#mission_detail_container").html(html);
+            contents.find("#mission_detail_container").html(html);
 
-    });
+        });
 
-  }
+    }
 
 })();
